306bf2c1103be7f48cef4fec212b4465b1069694
celebalane <celebalane@gmail.com>
2018-01-23 18:42:16 +0100

    ajout heure

--- app.js
+++ app.js
@@ -16,14 +16,24 @@ io.sockets.on('connection', function (socket, pseudo) {
 
     // Réception et envoi du message à tous
     socket.on('message', function (message) {
+    	var date = new Date();   //Récupération de l'heure d'envoi
+    	var heure   = ('0'+date.getHours()).slice(-2);
+		var minute  = ('0'+date.getMinutes()).slice(-2);
+		var seconde = ('0'+date.getSeconds()).slice(-2);
+		now = heure +':'+minute+':'+seconde;
         message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message ,date: now});
     }); 
 
     // Réception et envoi sur la page de l'envoyeur
     socket.on('myMessage', function (message) {
+    	var date = new Date();   //Récupération de l'heure d'envoi
+    	var heure   = ('0'+date.getHours()).slice(-2);
+		var minute  = ('0'+date.getMinutes()).slice(-2);
+		var seconde = ('0'+date.getSeconds()).slice(-2);
+		now = heure +':'+minute+':'+seconde;
         myMessage = ent.encode(message);
-        socket.emit('myMessage', {pseudo: socket.pseudo, message: myMessage});
+        socket.emit('myMessage', {pseudo: socket.pseudo, message: myMessage, date: now});
     }); 
 
     // Message de déconnexion envoyé à tout le monde
--- public/client.js
+++ public/client.js
@@ -6,24 +6,25 @@
             var pseudo = $('#pseudo').val().trim();
             socket.emit('newUser', pseudo);
             document.title = pseudo + ' - ' + document.title;
-            $('#title').text(pseudo + ' bienvenue sur le Chat');
+            $('#title').html("<span>"+ pseudo + '</span> bienvenue sur le Chat');
             $('section').removeClass('invisible');
-            $('#login').addClass('invisible');
+            $('#login').remove();
         });     
     }
 
     // Insère un message dans la page
-    function insereMessage(pseudo,message){
-        $('#zone_chat').prepend('<p><strong>' + pseudo + '</strong> ' + message + '</p>');
+    function insereMessage(pseudo,message,date){
+        $('#zone_chat').append('<p><strong>['+date+'] ' + pseudo + ' : </strong> ' + message + '</p>');
+        scrollToBottom();
     }
 
     // Affichage du message à tout le monde sauf à l'envoyeur
     socket.on('message', function(message){
-        insereMessage(message.pseudo, message.message);
+        insereMessage(message.pseudo, message.message, message.date);
     });
     // Affichage du message à l'envoyeur
     socket.on('myMessage', function(myMessage){
-        insereMessage(myMessage.pseudo, myMessage.message);
+        insereMessage(myMessage.pseudo, myMessage.message, myMessage.date);
     });
 
     // Envoi d'un message à tous
@@ -37,9 +38,15 @@
 
     // Message Connexion
     socket.on('newUser', function(pseudo) {
-        $('#zone_chat').prepend('<p><em>' + pseudo + ' a rejoint le Chat!</em></p>');
+        $('#zone_chat').append('<p><em>' + pseudo + ' a rejoint le Chat!</em></p>');
     });
     // Message Déconnexion
     socket.on('logout', function(message){
-        $('#zone_chat').prepend(message.text);
+        $('#zone_chat').append(message.text);
     });
+
+    function scrollToBottom() { // A faire fonctionner
+        if ($('#zone_chat').scrollTop() + $('#zone_chat').height() + 2 * $('#zone_chat p').last().outerHeight() >= $(document).height()) {
+            $("html, body").animate({ scrollTop: $(document).height() }, 0);
+        }
+    }
